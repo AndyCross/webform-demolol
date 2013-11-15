ad75eaa7247400bbc6576b09b78eeb9039fd26de
AndyCross <andy@elastacloud.com>
2013-11-15 12:20:16 +0000

    Initial Commit

---
+++ BrownBag/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BrownBag
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.TraceError("This is webforms. FAIL");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            int i;
+
+            if (int.TryParse(((Button)sender).Text, out i))
+            {
+                i++;
+            }
+            else
+            {
+                i = 0;
+            }
+            ((Button)sender).Text = i.ToString();
+        }
+    }
+}
---
+++ BrownBag/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BrownBag.Global" Language="C#" %>
